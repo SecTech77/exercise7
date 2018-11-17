f9fea53ebc7e5d707fb3ac585abfa9b732a0736b
Kacper <k.gracki@inteliclinic.com>
2018-11-17 14:23:15 +0100

    Cleaning up again

--- dirlister.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def run(**arg):
-
-    print("[*] Inside dirlister module")
-    files = os.listdir(".")
-
-    return str(files)
--- environment.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def run(**args):
-    print("[*] Inside environment module")
-
-    return str(os.environ)
